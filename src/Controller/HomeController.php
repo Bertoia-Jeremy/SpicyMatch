97da2d9778b99fee3d6a3f8d4d2c865e4184c41f
Bertoia-Jeremy <bibibrice@hotmail.fr>
2023-05-26 14:11:06 +0200

    homeController + template - beginning

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Repository\SpicesRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -9,12 +10,23 @@ use Symfony\Component\Routing\Annotation\Route;
 class HomeController extends AbstractController
 {
     /**
+     * @var SpicesRepository
+     */
+    private $spicesRepository;
+
+    public function __construct(SpicesRepository $spicesRepository)
+    {
+        $this->spicesRepository = $spicesRepository;
+    }
+
+    /**
      * @Route("/", name="home")
      */
     public function index(): Response
     {
+        $spices = $this->spicesRepository->findAll();
         return $this->render('home/index.html.twig', [
-            'controller_name' => 'HomeController',
+            'spices' => $spices,
         ]);
     }
 }

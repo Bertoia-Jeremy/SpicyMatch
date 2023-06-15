7043d0e95a37cd249e8a0e56ae1c0339249bd9e4
Bertoia-Jeremy <bertoia.jeremy@hotmail.fr>
2023-06-15 09:41:51 +0200

    AJAX beginning

--- src/Controller/SpicyMatchController.php
+++ src/Controller/SpicyMatchController.php
@@ -2,8 +2,10 @@
 
 namespace App\Controller;
 
+use App\Repository\AromaticCompoundRepository;
 use App\Repository\SpicesRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -16,8 +18,9 @@ class SpicyMatchController extends AbstractController
      * @var SpicesRepository
      */
     private $spicesRepository;
-
-    public function __construct(SpicesRepository $spicesRepository)
+    public function __construct(
+        SpicesRepository $spicesRepository
+    )
     {
         $this->spicesRepository = $spicesRepository;
     }
@@ -25,11 +28,45 @@ class SpicyMatchController extends AbstractController
     /**
      * @Route("/", name="index_spicy_match")
      */
-    public function index(): Response
+    public function index(Request $request): Response
     {
-        $spices = $this->spicesRepository->findAll();
-        return $this->render('spicy_match/index.html.twig', [
-            'spices' => $spices,
-        ]);
+        //Prendre exemple sur adminUserController dans le projet de la billeterie
+        //admin/user/index.html.twig
+        // Pour la macro : https://stackoverflow.com/questions/23315104/putting-twig-generates-html-into-a-js-variable
+        if ($request->isXmlHttpRequest()) {
+
+        } else {
+            $spices = $this->spicesRepository->findAll();
+
+            return $this->render('spicy_match/index.html.twig', [
+                'spices' => $spices,
+            ]);
+        }
+    }
+
+    /**
+     * @Route("/matcher/", name="view_spicy_match")
+     */
+    public function matcherView(Request $request)
+    {
+        $token = $request->request->get('_token');
+
+        if (is_string($token) && $this->isCsrfTokenValid('matcher', $token)) {
+            // $this->userRepository->remove($user, true);
+            return $this->json(['success' => true]);
+        }
+        /*   foreach ()
+           $spice = $this->spicesRepository->findBy(['id']);
+           $jsonData = array();
+           $idx = 0;
+           foreach($students as $student) {
+               $temp = array(
+                   'name' => $student->getName(),
+                   'address' => $student->getAddress(),
+               );
+               $jsonData[$idx++] = $temp;
+           }
+        */
+        return $this->json(['oki Doki' => 'cbon']);
     }
 }

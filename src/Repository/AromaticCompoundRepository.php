2bcba097936bf35e9d1993ab62650963d123a015
Bertoia-Jeremy <bertoia.jeremy@hotmail.fr>
2023-10-27 12:50:12 +0200

    clean

--- src/Repository/AromaticCompoundRepository.php
+++ src/Repository/AromaticCompoundRepository.php
@@ -38,29 +38,4 @@ class AromaticCompoundRepository extends ServiceEntityRepository
             $this->getEntityManager()->flush();
         }
     }
-
-//    /**
-//     * @return AromaticCompound[] Returns an array of AromaticCompound objects
-//     */
-//    public function findByExampleField($value): array
-//    {
-//        return $this->createQueryBuilder('a')
-//            ->andWhere('a.exampleField = :val')
-//            ->setParameter('val', $value)
-//            ->orderBy('a.id', 'ASC')
-//            ->setMaxResults(10)
-//            ->getQuery()
-//            ->getResult()
-//        ;
-//    }
-
-//    public function findOneBySomeField($value): ?AromaticCompound
-//    {
-//        return $this->createQueryBuilder('a')
-//            ->andWhere('a.exampleField = :val')
-//            ->setParameter('val', $value)
-//            ->getQuery()
-//            ->getOneOrNullResult()
-//        ;
-//    }
 }

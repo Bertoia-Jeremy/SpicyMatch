83afe37de44759d96be11f8ca04913e2278098f0
Jérémy.B <bibibrice@hotmail.fr>
2023-11-06 12:09:16 +0100

    fix

--- src/Form/ContactType.php
+++ src/Form/ContactType.php
@@ -6,7 +6,7 @@ use App\Entity\Contact;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\EmailType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
-use Symfony\Component\Form\Extension\Core\Type\TextAreaType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
@@ -19,7 +19,7 @@ class ContactType extends AbstractType
             ->add('email', EmailType::class, [ 'label' => 'Mail'])
             ->add('name', TextType::class, [ 'label' => 'Nom'])
             ->add('subject', TextType::class, [ 'label' => 'Sujet'])
-            ->add('message', TextAreaType::class, [ 'label' => 'Message'])
+            ->add('message', TextareaType::class, [ 'label' => 'Message'])
             ->add('Valider', SubmitType::class)
         ;
     }

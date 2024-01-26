92f0b31303c29069e5b206a0ae454ebf7dd22788
Jérémy.B <bibibrice@hotmail.fr>
2024-01-26 11:37:45 +0100

    initialize symfony turbo

--- config/bundles.php
+++ config/bundles.php
@@ -20,4 +20,5 @@ return [
     Symfony\UX\TwigComponent\TwigComponentBundle::class => ['all' => true],
     Symfony\UX\TogglePassword\TogglePasswordBundle::class => ['all' => true],
     Symfony\UX\LiveComponent\LiveComponentBundle::class => ['all' => true],
+    Symfony\UX\Turbo\TurboBundle::class => ['all' => true],
 ];

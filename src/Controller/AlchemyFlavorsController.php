5f21ae413ee59c58890114c649ba0326f01de134
Bertoia-Jeremy <bertoia.jeremy@hotmail.fr>
2024-02-01 11:51:13 +0100

    1st step view compounds & flavors - add link search + spice show

--- src/Controller/AlchemyFlavorsController.php
+++ src/Controller/AlchemyFlavorsController.php
@@ -4,6 +4,7 @@ declare(strict_types=1);
 
 namespace App\Controller;
 
+use App\Entity\AlchemyFlavors;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -16,6 +17,14 @@ class AlchemyFlavorsController extends AbstractController
     {
         return $this->render('alchemy_flavors/index.html.twig', [
             'controller_name' => 'AlchemyFlavorsController',
+        ]);
+    }
+
+    #[Route('/{id}', name: 'view_alchemy_flavors')]
+    public function view(AlchemyFlavors $alchemyFlavor): Response
+    {
+        return $this->render('alchemy_flavors/view.html.twig', [
+            'alchemyFlavor' => $alchemyFlavor,
         ]);
     }
 }
--- src/Controller/AromaticCompoundController.php
+++ src/Controller/AromaticCompoundController.php
@@ -4,6 +4,7 @@ declare(strict_types=1);
 
 namespace App\Controller;
 
+use App\Entity\AromaticCompound;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -16,6 +17,14 @@ class AromaticCompoundController extends AbstractController
     {
         return $this->render('aromatic_compound/index.html.twig', [
             'controller_name' => 'AromaticCompoundController',
+        ]);
+    }
+
+    #[Route('/{id}', name: 'view_aromatic_compound')]
+    public function view(AromaticCompound $aromaticCompound): Response
+    {
+        return $this->render('aromatic_compound/view.html.twig', [
+            'aromaticCompound' => $aromaticCompound,
         ]);
     }
 }

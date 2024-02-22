6784253c72a46d4c6b800a101363669862a6be2e
Jérémy.B <bibibrice@hotmail.fr>
2024-02-22 09:43:35 +0100

    change view home to spice view

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -4,35 +4,16 @@ declare(strict_types=1);
 
 namespace App\Controller;
 
-use App\Repository\SpicesRepository;
-use Knp\Component\Pager\PaginatorInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class HomeController extends AbstractController
 {
-    private \App\Repository\SpicesRepository $spicesRepository;
-
-    public function __construct(SpicesRepository $spicesRepository)
-    {
-        $this->spicesRepository = $spicesRepository;
-    }
-
     #[Route('/', name: 'home')]
-    public function index(Request $request, PaginatorInterface $paginator): Response
+    public function index(): Response
     {
-        $spicesAll = $this->spicesRepository->findAll();
-
-        $spices = $paginator->paginate(
-            $spicesAll,
-            $request->query->getInt('page', 1),
-            8
-        );
-
         return $this->render('home/index.html.twig', [
-            'spices' => $spices,
         ]);
     }
 }

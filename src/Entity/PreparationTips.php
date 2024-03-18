3a7875085b18926b9b2d4442d7070c9df491328c
Bertoia-Jeremy <bertoia.jeremy@hotmail.fr>
2024-03-18 12:03:05 +0100

    fix/refacto

--- src/Entity/PreparationTips.php
+++ src/Entity/PreparationTips.php
@@ -5,10 +5,8 @@ namespace App\Entity;
 use App\Repository\PreparationTipsRepository;
 use Doctrine\DBAL\Types\Types;
 use Doctrine\ORM\Mapping as ORM;
-use Symfony\UX\Turbo\Attribute\Broadcast;
 
 #[ORM\Entity(repositoryClass: PreparationTipsRepository::class)]
-#[Broadcast]
 class PreparationTips
 {
     #[ORM\Id]
